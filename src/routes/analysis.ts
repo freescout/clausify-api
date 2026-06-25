170bcf64541fd94b9df665d821f6d3b0363ba88b
Neethu <neethu.jacob@shadline.com>
2026-06-25 17:38:38 +0200

    feat: remove authenticate middleware

--- src/routes/analysis.ts
+++ src/routes/analysis.ts
@@ -1,13 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { analyzeHandler } from "../controllers/analysis";
-
 import { AnalyzeBody } from "../types";
-import { authenticate } from "../middlewares/auth";
 
 export async function analysisRoutes(fastify: FastifyInstance) {
-  fastify.post<{ Body: AnalyzeBody }>(
-    "/analyze",
-    { preHandler: [authenticate] },
-    analyzeHandler,
-  );
+  fastify.post<{ Body: AnalyzeBody }>("/analyze", analyzeHandler);
 }
